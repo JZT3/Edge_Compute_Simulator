5e8089e2263df991c05d473a6c2a065c6a867d12
Jervani <jzt@Jervanis-MacBook-Air.local>
2026-05-19 14:18:12 -0700

    Model of a communication link between two nodes, carrying channel‑dependent state

---
+++ simulator/include/sim/core/link.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "types.hpp"
+
+namespace sigint_sim {
+
+class Link {
+public:
+    Link(LinkId id, NodeId from, NodeId to, Frequency band_center);
+
+    void updateFromChannel(double snr, double capacity, double outage_prob, bool active);
+
+    [[nodiscard]] LinkState getState() const noexcept;
+    [[nodiscard]] LinkId id() const noexcept { return id_; }
+    [[nodiscard]] NodeId from() const noexcept { return from_; }
+    [[nodiscard]] NodeId to() const noexcept { return to_; }
+
+private:
+    LinkId id_;
+    NodeId from_;
+    NodeId to_;
+    Frequency band_center_;
+    double snr_ = 0.0;
+    double capacity_bps_ = 0.0;
+    double outage_prob_ = 1.0;
+    bool active_ = false;
+};
+
+} // namespace sigint_sim
