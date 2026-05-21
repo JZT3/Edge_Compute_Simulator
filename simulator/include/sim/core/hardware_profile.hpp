4cf5e8dc43a8c42146d3cb473307004a353d85ae
Jervani <jzt@Jervanis-MacBook-Air.local>
2026-05-21 02:01:28 -0700

    Encodes heterogeneous node capabilities; used by channel (noise figure) and signal processor (compute)

--- simulator/include/sim/core/hardware_profile.hpp
+++ simulator/include/sim/core/hardware_profile.hpp
@@ -1,13 +1,12 @@
 #pragma once
-#include <string>
-
-namespace sigint_sim {
+enum class DeviceType { RTL_SDR, HACKRF, LIME_SDR, USRP_B2XX, USRP_X3XX, VIRTUAL };
 
 struct HardwareProfile {
-    std::string device_string = "virtual";
+    DeviceType type = DeviceType::VIRTUAL;
     double noise_figure_dB = 10.0;
     double tx_power_dBm = 10.0;
     double frequency_accuracy_ppm = 1.0;
+    // Compute capabilities
+    double fft_gflops_per_sec = 1.0;   // GFLOPS available for detection
+    double memory_mib = 1024.0;
 };
-
-} // namespace sigint_sim
